8bdb27ce2422ae7298f29115e9d56dfc6f6b1d6e
Vinay Mishra <1841084+mishravinay@users.noreply.github.com>
2023-12-06 04:02:38 +0000

    First commit

---
+++ 01Basics/00_Variables.js
@@ -0,0 +1,25 @@
+// Print the Value in console
+console.log("First Console Value - Hello Vinay");
+
+const pi = 3.14;
+let name = "Vinay Mishra";
+var userCity = "Faridabad";
+userAddress = "15840 Apt- t2";
+
+console.log (pi);
+console.table ([pi, name, userCity, userAddress]);
+
+//pi = 3.14321 // TypeError: Assignment to constant variable. Constant value can't be changed
+name = "VM";
+userCity = "Lucknow";
+
+userAddress = "132846 Apt-T4"
+
+console.table ([pi, name, userCity, userAddress]);
+
+/**
+ * Difference between let, Var and Const
+ * What are the major difference between let and Var 
+ * What was the problem with Var?
+ */
+
